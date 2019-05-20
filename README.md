35877c3bf9ca3e7194f521a326096dd36ef34914
Samarth Agarwal <samarthagarwal@live.com>
2019-05-20 19:59:03 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# FutureBuilder in Flutter
+
+This project contains the source code for the YouTube video [here](https://www.youtube.com/watch?v=EwHMSxSWIvQ).
+
+The video teaches how to use FutureBuilder in a Flutter app by fetching data from a remote REST API or web service.
---
+++ lib/main.dart
@@ -0,0 +1,127 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+      title: 'Flutter Demo',
+      theme: new ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: new MyHomePage(title: 'Users'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  Future<List<User>> _getUsers() async {
+
+    var data = await http.get("http://www.json-generator.com/api/json/get/cfwZmvEBbC?indent=2");
+
+    var jsonData = json.decode(data.body);
+
+    List<User> users = [];
+
+    for(var u in jsonData){
+
+      User user = User(u["index"], u["about"], u["name"], u["email"], u["picture"]);
+
+      users.add(user);
+
+    }
+
+    print(users.length);
+
+    return users;
+
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
+      ),
+      body: Container(
+          child: FutureBuilder(
+            future: _getUsers(),
+            builder: (BuildContext context, AsyncSnapshot snapshot){
+              print(snapshot.data);
+              if(snapshot.data == null){
+                return Container(
+                  child: Center(
+                    child: Text("Loading...")
+                  )
+                );
+              } else {
+                return ListView.builder(
+                  itemCount: snapshot.data.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return ListTile(
+                      leading: CircleAvatar(
+                        backgroundImage: NetworkImage(
+                          snapshot.data[index].picture
+                        ),
+                      ),
+                      title: Text(snapshot.data[index].name),
+                      subtitle: Text(snapshot.data[index].email),
+                      onTap: (){
+
+                        Navigator.push(context, 
+                          new MaterialPageRoute(builder: (context) => DetailPage(snapshot.data[index]))
+                        );
+
+                      },
+                    );
+                  },
+                );
+              }
+            },
+          ),
+        ),
+      );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+
+  final User user;
+
+  DetailPage(this.user);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text(user.name),
+        )
+    );
+  }
+}
+
+
+class User {
+  final int index;
+  final String about;
+  final String name;
+  final String email;
+  final String picture;
+
+  User(this.index, this.about, this.name, this.email, this.picture);
+
+}
